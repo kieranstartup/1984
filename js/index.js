fd9a14e0b442dc1b2e8c21ac12e974772d6c2142
Kieran Startup <info@kieranstartup.co.uk>
2016-05-18 15:56:35 +0100

    upppppppdates

--- js/index.js
+++ js/index.js
@@ -1,42 +1,56 @@
-jQuery(document).ready(function() {
+// jQuery(document).ready(function() {
 
 
-    function paragraphOneImages() {
+//     function paragraphImages() {
 
-        // var imgsArray = ["!", "amazon", "balloons", "Beauty", "bikini body", "buy now"];
-        var numberOfFaces = 5;
-        var paragraph3 = document.getElementById("p3");
-
-        for (var i = 0; i < numberOfFaces; i++) {
-            var img = document.createElement("img");
-
-            img.src = "popups/amazon.png";
-
-            var topLoc = Math.floor(Math.random() * 400);
-            var botLoc = Math.floor(Math.random() * 400);
-
-            // changes are here!
-            img.style.top = topLoc + "px"; // added px
-            img.style.left = botLoc + "px"; // and used "left" instead of "bottom"
-            paragraph3.appendChild(img);
-        }
-    }
-    paragraphOneImages();
-
-
-    jQuery(document.body).on('click', 'img', function() {
-        jQuery(this).remove();
-    });
-
-    // function showLetter() {
-    //     var letter = imgsArray[generateRandomForArray()];
-    //     jQuery(".page").append("<img src='popups/" + letter + ".png'>");
-    //     var left = generateRandom();
-    //     var top = generateRandom();
-    //     jQuery(".page").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
-    // }
+//         // var imgsArray = ["!", "amazon", "balloons", "Beauty", "bikini body", "buy now"];
+//         var numberOfFaces = 5;
+        
+//         var paragraph3 = document.getElementById("p3");
+//         var paragraph4 = document.getElementById("p4");
+//         var paragraph5 = document.getElementById("p5");
+//         var paragraph6 = document.getElementById("p6");
+//         var paragraph7 = document.getElementById("p7");
+//         var paragraph8 = document.getElementById("p8");
+//         var paragraph9 = document.getElementById("p9");
+//         var paragraph10 = document.getElementById("p10");
+//         var paragraph11 = document.getElementById("p11");
+//         var paragraph12 = document.getElementById("p12");
+//         var paragraph13 = document.getElementById("p13");
+//         var paragraph14 = document.getElementById("p14");
+//         var paragraph15 = document.getElementById("p15");
 
 
 
+//         for (var i = 0; i < numberOfFaces; i++) {
+//             var img = document.createElement("img");
 
-});
+//             img.src = "popups/amazon.png";
+
+//             var topLoc = Math.floor(Math.random() * 400);
+//             var botLoc = Math.floor(Math.random() * 400);
+
+//             img.style.top = topLoc + "px";
+//             img.style.left = botLoc + "px";
+//             paragraph3.appendChild(img);
+//         }
+//     }
+//     paragraphImages();
+
+
+//     jQuery(document.body).on('click', 'img', function() {
+//         jQuery(this).remove();
+//     });
+
+//     // function showLetter() {
+//     //     var letter = imgsArray[generateRandomForArray()];
+//     //     jQuery(".page").append("<img src='popups/" + letter + ".png'>");
+//     //     var left = generateRandom();
+//     //     var top = generateRandom();
+//     //     jQuery(".page").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
+//     // }
+
+
+
+
+// });
