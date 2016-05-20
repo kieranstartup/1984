39225a688096575b6e778ee45aabe1ffb97c61d9
Kieran Startup <info@kieranstartup.co.uk>
2016-05-20 15:09:58 +0100

    still not figured it out

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-jQuery(window).load(function() {
+// jQuery(window).load(function() {
 
-    jQuery('img.popups').bind('inview', function(event, visible) {
-        if (visible === true) {
-            jQuery(this).fadeIn(5000);
-            console.log("this is faded in " + this);
-        } else {
-            jQuery(this).fadeIn(5000);
-            console.log("this is faded out " + this);
-        }
-    });
+//     jQuery(document.body).on('scroll', function() { //Change to Touchstart
 
-});
+//     jQuery('img.popups').bind('inview', function(event, visible) {
+//         if (visible === true) {
+//             jQuery(this).fadeIn(500);
+//             console.log("this is faded in " + this);
+//         } else {
+//             jQuery(this).fadeIn(500);
+//             console.log("this is faded out " + this);
+//         }
+//     });
+
+// });
+
+// });
